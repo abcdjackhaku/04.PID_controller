4fd29ef2aaba376dabf37a162b10f337c7aff5f5
HAKUSHO CHIN <40905044+abcdjackhaku@users.noreply.github.com>
2019-03-10 15:26:38 +0900

    Add files via upload

---
+++ src/PID.cpp
@@ -0,0 +1,41 @@
+#include "PID.h"
+
+/**
+ * TODO: Complete the PID class. You may add any additional desired functions.
+ */
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+  /**
+   * TODO: Initialize PID coefficients (and errors, if needed)
+   */
+  	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+
+	p_error = 0;
+	i_error = 0;
+	d_error = 0;
+
+}
+
+void PID::UpdateError(double cte) {
+  /**
+   * TODO: Update PID errors based on cte.
+   */
+  	double previous_cte = p_error;
+	p_error = cte;
+	i_error += cte;
+	d_error = cte - previous_cte;
+
+}
+
+double PID::TotalError() {
+  /**
+   * TODO: Calculate and return the total error
+   */
+  return -Kp*p_error - Ki*i_error - Kd*d_error;
+}
